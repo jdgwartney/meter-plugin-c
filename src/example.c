00d6aec96044d1f48809b86654e33158812070d9
David Gwartney <davidg@boundary.com>
2016-06-10 11:13:11 -0700

    Add stubs for example plugin

---
+++ src/example.c
@@ -0,0 +1,5 @@
+//
+// Created by Gwartney, David on 6/10/16.
+//
+
+#include "example.h"
---
+++ src/example.h
@@ -0,0 +1,8 @@
+//
+// Created by Gwartney, David on 6/10/16.
+//
+
+#ifndef METER_PLUGIN_EXAMPLE_H
+#define METER_PLUGIN_EXAMPLE_H
+
+#endif //METER_PLUGIN_EXAMPLE_H
